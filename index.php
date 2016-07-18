d6fdf702e0c60666e5944688cab6dd69eb3c95c0
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-07-19 07:14:11 +0800

    Splashscreen fix

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         
         <!--Splash-->
         <!-- iPhone 5 startup image -->
-        <link href="utbmoc_splash-640x1096.png"
+        <link href="splash/utbmoc_splash-640x1096.png"
           media="(device-width: 320px) and (device-height: 568px)
                  and (-webkit-device-pixel-ratio: 2)"
           rel="apple-touch-startup-image">
