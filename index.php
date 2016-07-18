eed124c09799aa933735cfe153951519c09c6f40
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-07-18 18:47:55 +0800

    Raquo & cosmetics

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
         <title>Checklist</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="style.css" />        
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Maven+Pro:400,900" rel="stylesheet">
         <link rel="icon" href="http://www.utb.edu.bn/favicon.ico" type="image/x-icon">
     </head>
     <body>
@@ -22,9 +21,17 @@
                     </div>
                     <div class="moc-canvas">
                         <div class="moc-post">
-                            <h3>Day 1</h3>
+                            <h3>&raquo; Day 1</h3>
                             <p>
                                 <ul>                                    
+                                    <li>                                        
+                                        <h4>
+                                            <input type="checkbox" name="moc-step" value="step" checked="checked">
+                                            STEP #0 - Come to UTB
+                                        </h4>
+                                        <p></p>
+                                    </li>
+                                        <hr>
                                     <li>                                        
                                         <h4>
                                             <input type="checkbox" name="moc-step" value="step">
@@ -71,7 +78,46 @@
                                             <input type="checkbox" name="moc-step" value="step">
                                             STEP #5 - COLLECT Orientation Pack
                                         </h4>
-                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                        <p>You should have:
+                                            <ol>
+                                                <li>
+                                                    <input type="checkbox" name="moc-op" value="shirt">
+                                                    Official UTB Shirt
+                                                </li>
+                                                <li>
+                                                    <input type="checkbox" name="moc-op" value="shirt">
+                                                    Rumpun UTB Shirt
+                                                </li>
+                                                <li>
+                                                    <input type="checkbox" name="moc-op" value="shirt">
+                                                    UTB Badge
+                                                </li>
+                                                <li>
+                                                    <input type="checkbox" name="moc-op" value="shirt">
+                                                    UTB String bag
+                                                </li>
+                                                <li>
+                                                    <input type="checkbox" name="moc-op" value="shirt">
+                                                    Umbrella
+                                                </li>
+                                                <li>
+                                                    <input type="checkbox" name="moc-op" value="shirt">
+                                                    Pen
+                                                </li>
+                                                <li>
+                                                    <input type="checkbox" name="moc-op" value="shirt">
+                                                    UTB Lanyard
+                                                </li>
+                                                <li>
+                                                    <input type="checkbox" name="moc-op" value="shirt">
+                                                    UTB Notebook*
+                                                </li>
+                                                <li>
+                                                    <input type="checkbox" name="moc-op" value="shirt">
+                                                    Car shade
+                                                </li>
+                                            </ol>
+                                        </p>
                                     </li>
                                         <hr>
                                     <li>
@@ -79,21 +125,22 @@
                                             <input type="checkbox" name="moc-step" value="step">
                                             STEP #6 - VISIT CCA Booths
                                         </h4>
-                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                        <p>Location:</p>
+                                        <p>Head over to the Silver Jubilee Court and enjoy some astonishing performances from our clubs prepared just for you! Don't forget to sign up for your favorite clubs as well. See you there!</p>
                                     </li>
                                 </ul>
                             </p>
                         </div>
                         <div class="moc-post">
-                            <h3>Day 2</h3>
+                            <h3>&raquo; Day 2</h3>
                             <p></p>
                         </div>
                         <div class="moc-post">
-                            <h3>Day 3</h3>
+                            <h3>&raquo; Day 3</h3>
                             <p></p>
                         </div>
                         <div class="moc-post">
-                            <h3>Day 4</h3>
+                            <h3>&raquo; Day 4</h3>
                             <p></p>
                         </div>
                     </div>
