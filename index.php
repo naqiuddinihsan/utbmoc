8679650b4d310ff7843312ed72fd88e49ea63fa4
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-07-18 18:23:14 +0800

    More cosmetics

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>UTB MO Checklist</title>
+        <title>Checklist</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="style.css" />        
         <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
@@ -18,34 +18,70 @@
                 </div>
                 <div class="moc-mid">
                     <div class="moc-banner">
-
+                        <p>Check out pmutb.Slack.com!</p>
                     </div>
                     <div class="moc-canvas">
                         <div class="moc-post">
                             <h3>Day 1</h3>
                             <p>
-                                <ol>                                    
-                                    <li>
-                                        <input type="checkbox" name="moc-step" value="step">
-                                        <h4>OPEN bank account</h4>
+                                <ul>                                    
+                                    <li>                                        
+                                        <h4>
+                                            <input type="checkbox" name="moc-step" value="step">
+                                            STEP #1 - OPEN Bank account
+                                        </h4>
                                         <p>For allowance purposes (applicable to Government Scholarship students only), if you do not have a Bank Account, please go to a bank and open up a new account</p>
                                         <p>If you already have a Bank account <br>OR<br> a Non-government Student, please proceed to Step 2</p>
                                     </li>
-                                    <hr>
+                                        <hr>
                                     <li>
-                                        <input type="checkbox" name="moc-step" value="step">
-                                        GET Insurance</li>
+                                        <h4>
+                                            <input type="checkbox" name="moc-step" value="step">
+                                            STEP #2 - GET Insurance
+                                        </h4>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                    </li>
+                                        <hr>
                                     <li>
-                                        <input type="checkbox" name="moc-step" value="step">CREATE Student ID</li>
+                                        <h4>
+                                            <input type="checkbox" name="moc-step" value="step">
+                                            STEP #3A - CREATE Student ID
+                                        </h4>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                    </li>
+                                        <hr>
                                     <li>
-                                        <input type="checkbox" name="moc-step" value="step">REGISTER course</li>
+                                        <h4>
+                                            <input type="checkbox" name="moc-step" value="step">
+                                            STEP #3B - REGISTER Course
+                                        </h4>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                    </li>
+                                        <hr>
                                     <li>
-                                        <input type="checkbox" name="moc-step" value="step">PAY Fees</li>
+                                        <h4>
+                                            <input type="checkbox" name="moc-step" value="step">
+                                            STEP #4 - PAY Fees
+                                        </h4>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                    </li>
+                                        <hr>
                                     <li>
-                                        <input type="checkbox" name="moc-step" value="step">COLLECT Orientation Pack</li>
+                                        <h4>
+                                            <input type="checkbox" name="moc-step" value="step">
+                                            STEP #5 - COLLECT Orientation Pack
+                                        </h4>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                    </li>
+                                        <hr>
                                     <li>
-                                        <input type="checkbox" name="moc-step" value="step">VISIT CCA Booths</li>
-                                </ol>
+                                        <h4>
+                                            <input type="checkbox" name="moc-step" value="step">
+                                            STEP #6 - VISIT CCA Booths
+                                        </h4>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                    </li>
+                                </ul>
                             </p>
                         </div>
                         <div class="moc-post">
