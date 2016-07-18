0f22a296c76d8c0bb907308bf333bb92eea6439a
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-07-18 19:33:55 +0800

    main page push

--- index.php
+++ index.php
@@ -24,60 +24,117 @@
                             <h3>&raquo; Day 1</h3>
                             <p>
                                 <ul>                                    
-                                    <li>                                        
+                                    <li>
+                                        <div class="moc-step">
                                         <h4>
                                             <input type="checkbox" name="moc-step" value="step" checked="checked">
                                             STEP #0 - Come to UTB
                                         </h4>
                                         <p></p>
+                                        </div>
                                     </li>
-                                        <hr>
-                                    <li>                                        
+                                        
+                                    
+                                    
+                                    <li>
+                                        <div class="moc-step">
                                         <h4>
                                             <input type="checkbox" name="moc-step" value="step">
-                                            STEP #1 - OPEN Bank account
+                                            STEP #1 - OPEN Bank Account
                                         </h4>
+                                        <h5>Location: ???</h5>
                                         <p>For allowance purposes (applicable to Government Scholarship students only), if you do not have a Bank Account, please go to a bank and open up a new account</p>
+                                        <p>If you already have a Bank account <b>OR</b> a Non-government Student, <br>please proceed to <b>Step 2</b></p>
+                                        </div>
                                     </li>
-                                        <hr>
+                                        
+                                        
                                         
                                     <li>
+                                        <div class="moc-step">
                                         <h4>
                                             <input type="checkbox" name="moc-step" value="step">
                                             STEP #2 - GET Insurance
                                         </h4>
+                                        <h5>Location: GF.30</h5>
+                                        <p>All new students are advised to pay for insurance according to their Faculty/School before proceeding to <b>Step 3</b></p>
+                                        
+                                        <h5>Faculty of Engineering &raquo; Takaful Brunei Keluarga</h5>
+                                        <h5>School of Business &amp; School of Computing and Informatics &raquo; Insurans Islam TAIB</h5>                                        
+                                        </div>   
                                     </li>
-                                        <hr>
+                                        
+                                        <li>
+                                        <div class="moc-hint"
+                                            <p><em>You may either complete STEP 3A or 3B in any order. However, you still have to complete both steps before proceeding to <b>Step 4</b></em></p>
+                                        </div>
+                                        </li>
+                                        
                                     <li>
+                                        <div class="moc-step">
                                         <h4>
                                             <input type="checkbox" name="moc-step" value="step">
                                             STEP #3A - CREATE Student ID
                                         </h4>
-                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                        <h5>Location: GF.31</h5>
+                                        <p>To-bring:</p>
+                                        <input type="checkbox" name="moc-tobring3a" value="passphot"> Softcopy of passport-sized Photo
+                                        </div>
                                     </li>
-                                        <hr>
+                                        
+                                        
+                                        
+                                        
+                                        
                                     <li>
+                                        <div class="moc-step">
                                         <h4>
                                             <input type="checkbox" name="moc-step" value="step">
                                             STEP #3B - REGISTER Course
                                         </h4>
-                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                        <h5>Location: Teratak Putih</h5>
+                                        <p>To-bring:</p>
+                                        <ul>
+                                            <li><input type="checkbox" name="moc-tobring3b" value="tobring"> <em>Photocopy</em> of Offer Letter</li>
+                                            <li><input type="checkbox" name="moc-tobring3b" value="tobring"> Two (2x) Passport-sized photo</li>
+                                            <li><input type="checkbox" name="moc-tobring3b" value="tobring"> Two (2x) copies of ID Card</li>
+                                            <li><input type="checkbox" name="moc-tobring3b" value="tobring"> Insurance receipt</li>
+                                            <li><input type="checkbox" name="moc-tobring3b" value="tobring"> <em>Photocopy</em> of bank account details</li>
+                                        </ul>
+                                        </div>
                                     </li>
-                                        <hr>
+                                        
                                     <li>
+                                        <div class="moc-step">
                                         <h4>
                                             <input type="checkbox" name="moc-step" value="step">
                                             STEP #4 - PAY Fees
                                         </h4>
-                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                        <h5>Location: GF.27/28 &amp; GF.29</h5>
+                                        <p>To-bring:</p>
+                                        <ul>
+                                            <li><input type="checkbox" name="moc-tobring4" value="tobring"> IC</li>
+                                            <li><input type="checkbox" name="moc-tobring4" value="tobring"> Offer letter</li>
+                                            <li><input type="checkbox" name="moc-tobring4" value="tobring"> Money (Cash-in-hand)</li>       
+                                        </ul>
+                                        </div>
                                     </li>
-                                        <hr>
+                                        
                                     <li>
+                                        <div class="moc-step">
                                         <h4>
                                             <input type="checkbox" name="moc-step" value="step">
                                             STEP #5 - COLLECT Orientation Pack
                                         </h4>
-                                        <p>You should have:
+                                        <h5>Location: Concourse Hall</h5>
+                                        <p>To bring/submit:
+                                            <ul>
+                                                <li>
+                                                    *<input type="checkbox" name="moc-step" value="step"> Payment receipt (from <b>Step 4</b>)
+                                                </li>
+                                            </ul>
+                                        </p>
+                                        <p>Inside the orientation pack, you should have:
                                             <ol>
                                                 <li>
                                                     <input type="checkbox" name="moc-op" value="shirt">
@@ -117,19 +174,26 @@
                                                 </li>
                                             </ol>
                                         </p>
+                                        </div>
                                     </li>
-                                        <hr>
+                                        
+                        
+                        
+                        
                                     <li>
+                                        <div class="moc-step">
                                         <h4>
                                             <input type="checkbox" name="moc-step" value="step">
                                             STEP #6 - VISIT CCA Booths
                                         </h4>
-                                        <p>Location:</p>
+                                        <h5>Location: Silver Jubilee Court</h5>
                                         <p>Head over to the Silver Jubilee Court and enjoy some astonishing performances from our clubs prepared just for you! Don't forget to sign up for your favorite clubs as well. See you there!</p>
+                                        </div>
                                     </li>
                                 </ul>
                             </p>
                         </div>
+<!--
                         <div class="moc-post">
                             <h3>&raquo; Day 2</h3>
                             <p></p>
@@ -142,6 +206,7 @@
                             <h3>&raquo; Day 4</h3>
                             <p></p>
                         </div>
+-->
                     </div>
                 </div>
                 <div class="moc-bot"></div>
