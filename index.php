1fcb388b473333bfedee81f1e95db73def02d733
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-07-19 17:05:03 +0800

    construction page

--- index.php
+++ index.php
@@ -153,14 +153,6 @@
                                     <img data-u="image" src="img/UTBMOC_Banner_construction.jpg" />
                                 </div>                                
                             </div>
-                            <!-- Bullet Navigator -->
-                            <div data-u="navigator" class="jssorb05" style="bottom:16px;right:16px;" data-autocenter="1">
-                                <!-- bullet navigator item prototype -->
-                                <div data-u="prototype" style="width:16px;height:16px;"></div>
-                            </div>
-                            <!-- Arrow Navigator -->
-                            <span data-u="arrowleft" class="jssora22l" style="top:0px;left:12px;width:40px;height:58px;" data-autocenter="2"></span>
-                            <span data-u="arrowright" class="jssora22r" style="top:0px;right:12px;width:40px;height:58px;" data-autocenter="2"></span>
                         </div>  
                     </div>
             </div>
