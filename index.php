21802fb04478e9cbb6b3e0064dfd9c112b777a73
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-07-19 13:47:53 +0800

    Prevent overscroll fix

--- index.php
+++ index.php
@@ -17,9 +17,24 @@
         
         <!-- Prevent overscroll-->
         <script type="text/javascript">
-             $('body').on('touchmove', function (e) {
-                 if (!$('.scrollable').has($(e.target)).length) e.preventDefault();
-             });
+             var selScrollable = '.scrollable';
+            // Uses document because document will be topmost level in bubbling
+            $(document).on('touchmove',function(e){
+              e.preventDefault();
+            });
+            // Uses body because jQuery on events are called off of the element they are
+            // added to, so bubbling would not work if we used document instead.
+            $('body').on('touchstart', selScrollable, function(e) {
+              if (e.currentTarget.scrollTop === 0) {
+                e.currentTarget.scrollTop = 1;
+              } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
+                e.currentTarget.scrollTop -= 1;
+              }
+            });
+            // Stops preventDefault from being called on document if it sees a scrollable div
+            $('body').on('touchmove', selScrollable, function(e) {
+              e.stopPropagation();
+            });
         </script>        
     </head>
     <?php
