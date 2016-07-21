eff6f0dd728b35c3b9f31d7ffc1c1b28839553a4
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-07-21 17:12:57 +0800

    Banner test cache

--- index.php
+++ index.php
@@ -22,6 +22,19 @@
         <script type="text/javascript" src="js/jssor.slider.mini.js"></script>
         <script type="text/javascript" src="js/jssor.params.js"></script>
         <link rel="stylesheet" href="css/jssor.css">        
+        <style>
+            #jssor_1 {
+                position: relative;
+                margin: 0 auto;
+                top: 0px;
+                left: 0px; 
+/*                width: 1300px;*/
+                width: 100%;
+                height: 620px; 
+                overflow: hidden;
+                visibility: hidden;
+            }
+        </style>
     </head> 
     
     <!--Location data-->
@@ -49,12 +62,7 @@
                 <div id="randomdiv" class="moc-mid">                    
                     <!--AD BANNER-->
                     <div class="moc-banner">                        
-                        <div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 1300px; height: 620px; overflow: hidden; visibility: hidden;">
-                            <!-- Loading Screen -->
-                            <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
-                                <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
-                                <div style="position:absolute;display:block;background:url('img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
-                            </div>
+                        <div id="jssor_1">                            
                             
                             <!--SLIDES-->
                             <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 1300px; height: 620px; overflow: hidden;">
@@ -76,20 +84,8 @@
                                 </div>
                                 <div data-p="225.00" style="display: none;">                                    
                                         <img data-u="image" src="img/UTBMOC_Banner_passphot.jpg" />
-                                </div>
-                                
+                                </div>                                
                             </div>
-                            <!-- Bullet Navigator -->
-<!--
-                            <div data-u="navigator" class="jssorb05" style="bottom:16px;right:16px;" data-autocenter="1">
-                                <div data-u="prototype" style="width:16px;height:16px;"></div>
-                            </div>
--->
-                            <!-- Arrow Navigator -->
-<!--
-                            <span data-u="arrowleft" class="jssora22l" style="top:0px;left:12px;width:40px;height:58px;" data-autocenter="2"></span>
-                            <span data-u="arrowright" class="jssora22r" style="top:0px;right:12px;width:40px;height:58px;" data-autocenter="2"></span>
--->
                         </div>  
                     </div>
                     
