f4d378118aa6c6b33816d72b4b8bfc79777fe4a2
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-07-21 10:56:45 +0800

    Location updates

--- moc_data.php
+++ moc_data.php
@@ -1,9 +1,22 @@
 <?php
+    // Open Bank
     $moc_loc_s1 = "Respective banks";
-    $moc_loc_s2 = "GF.30";
-    $moc_loc_s3a = "GF.31";
-    $moc_loc_s3b = "Teratak Putih";
-    $moc_loc_s4 = "GF.27/28 and GF.29";
+
+    // Insurance
+    $moc_loc_s2 = "Concourse Hall";
+
+    //Student ID
+    $moc_loc_s3a = "???";
+
+    // Register course
+    $moc_loc_s3b = "Computer Labs(?)";
+
+    // Pay fees
+    $moc_loc_s4 = "GF.27/28";
+
+    // Collect orientation pack
     $moc_loc_s5 = "Concourse Hall";
-    $moc_loc_s6 = "Silver Jubilee Court";
+
+    // CCA Booths
+    $moc_loc_s6 = "Teratak Putih";
 ?>
