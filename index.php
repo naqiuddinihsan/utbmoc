4f1db5f8e34ff02d0949ad716bdd76b74a9b2deb
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-07-21 15:38:18 +0800

    cache fix

--- index.php
+++ index.php
@@ -21,8 +21,7 @@
         <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
         <script type="text/javascript" src="js/jssor.slider.mini.js"></script>
         <script type="text/javascript" src="js/jssor.params.js"></script>
-        <link rel="stylesheet" href="/css/jssor.css">
-        
+        <link rel="stylesheet" href="/css/jssor.css">        
     </head> 
     
     <!--Location data-->
