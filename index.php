9a36959f4725321d2de79376c7b56ee5c7db6afb
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-07-21 16:03:54 +0800

    remove all link for cache fix

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
         <title>Checklist</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="stylesheet" href="style.css" />        
-        <link href="https://fonts.googleapis.com/css?family=Maven+Pro:400,900" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Roboto:400,900" rel="stylesheet">
+        
         
         <!--Cosmetic-->
         <link rel="icon" href="favicon.ico" type="image/x-icon">
