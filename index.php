59b2efed49715b29fb2f27dd3436fc86982b1fdd
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-07-21 14:16:24 +0800

    Code cleaning

--- index.php
+++ index.php
@@ -14,122 +14,25 @@
         <link rel="apple-touch-icon" href="touch-icon-iphone-retina.png">
         <meta name="apple-mobile-web-app-status-bar-style" content="white">
         
+        <!--Litebox-->
+        <link rel="stylesheet" href="/css/lightbox.min.css">
+        
         <!--Slider-->
         <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
         <script type="text/javascript" src="js/jssor.slider.mini.js"></script>
-        <script>
-            jQuery(document).ready(function ($) {
-
-                var jssor_1_SlideoTransitions = [
-                  [{b:5500,d:3000,o:-1,r:240,e:{r:2}}],
-                  [{b:-1,d:1,o:-1,c:{x:51.0,t:-51.0}},{b:0,d:1000,o:1,c:{x:-51.0,t:51.0},e:{o:7,c:{x:7,t:7}}}],
-                  [{b:-1,d:1,o:-1,sX:9,sY:9},{b:1000,d:1000,o:1,sX:-9,sY:-9,e:{sX:2,sY:2}}],
-                  [{b:-1,d:1,o:-1,r:-180,sX:9,sY:9},{b:2000,d:1000,o:1,r:180,sX:-9,sY:-9,e:{r:2,sX:2,sY:2}}],
-                  [{b:-1,d:1,o:-1},{b:3000,d:2000,y:180,o:1,e:{y:16}}],
-                  [{b:-1,d:1,o:-1,r:-150},{b:7500,d:1600,o:1,r:150,e:{r:3}}],
-                  [{b:10000,d:2000,x:-379,e:{x:7}}],
-                  [{b:10000,d:2000,x:-379,e:{x:7}}],
-                  [{b:-1,d:1,o:-1,r:288,sX:9,sY:9},{b:9100,d:900,x:-1400,y:-660,o:1,r:-288,sX:-9,sY:-9,e:{r:6}},{b:10000,d:1600,x:-200,o:-1,e:{x:16}}]
-                ];
-
-                var jssor_1_options = {
-                  $AutoPlay: true,
-                  $SlideDuration: 800,
-                  $SlideEasing: $Jease$.$OutQuint,
-                  $CaptionSliderOptions: {
-                    $Class: $JssorCaptionSlideo$,
-                    $Transitions: jssor_1_SlideoTransitions
-                  },
-                  $ArrowNavigatorOptions: {
-                    $Class: $JssorArrowNavigator$
-                  },
-                  $BulletNavigatorOptions: {
-                    $Class: $JssorBulletNavigator$
-                  }
-                };
-
-                var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
-
-                //responsive code begin
-                //you can remove responsive code if you don't want the slider scales while window resizing
-                function ScaleSlider() {
-                    var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-                    if (refSize) {
-                        refSize = Math.min(refSize, 1920);
-                        jssor_1_slider.$ScaleWidth(refSize);
-                    }
-                    else {
-                        window.setTimeout(ScaleSlider, 30);
-                    }
-                }
-                ScaleSlider();
-                $(window).bind("load", ScaleSlider);
-                $(window).bind("resize", ScaleSlider);
-                $(window).bind("orientationchange", ScaleSlider);
-                //responsive code end
-            });
-        </script>
-        <link rel="stylesheet" href="/css/lightbox.min.css">
+        <script type="text/javascript" src="js/jssor.params.js"></script>
+        <link rel="stylesheet" href="/css/jssor.css">
         
-        <style>
-            /* jssor slider bullet navigator skin 05 css */
-            /*
-            .jssorb05 div           (normal)
-            .jssorb05 div:hover     (normal mouseover)
-            .jssorb05 .av           (active)
-            .jssorb05 .av:hover     (active mouseover)
-            .jssorb05 .dn           (mousedown)
-            */
-            .jssorb05 {
-                position: absolute;
-            }
-            .jssorb05 div, .jssorb05 div:hover, .jssorb05 .av {
-                position: absolute;
-                /* size of bullet elment */
-                width: 16px;
-                height: 16px;
-                background: url('img/b05.png') no-repeat;
-                overflow: hidden;
-                cursor: pointer;
-            }
-            .jssorb05 div { background-position: -7px -7px; }
-            .jssorb05 div:hover, .jssorb05 .av:hover { background-position: -37px -7px; }
-            .jssorb05 .av { background-position: -67px -7px; }
-            .jssorb05 .dn, .jssorb05 .dn:hover { background-position: -97px -7px; }
-
-            /* jssor slider arrow navigator skin 22 css */
-            /*
-            .jssora22l                  (normal)
-            .jssora22r                  (normal)
-            .jssora22l:hover            (normal mouseover)
-            .jssora22r:hover            (normal mouseover)
-            .jssora22l.jssora22ldn      (mousedown)
-            .jssora22r.jssora22rdn      (mousedown)
-            */
-            .jssora22l, .jssora22r {
-                display: block;
-                position: absolute;
-                /* size of arrow element */
-                width: 40px;
-                height: 58px;
-                cursor: pointer;
-                background: url('img/a22.png') center center no-repeat;
-                overflow: hidden;
-            }
-            .jssora22l { background-position: -10px -31px; }
-            .jssora22r { background-position: -70px -31px; }
-            .jssora22l:hover { background-position: -130px -31px; }
-            .jssora22r:hover { background-position: -190px -31px; }
-            .jssora22l.jssora22ldn { background-position: -250px -31px; }
-            .jssora22r.jssora22rdn { background-position: -310px -31px; }
-        </style>    
     </head>
+    
+    <!--Location data-->
     <?php
-        if (include("moc_data.php"))
-            include("moc_data.php");
+        if (include("/res/moc_data.php"))
+            include("/res/moc_data.php");
         else
         $moc_loc_s1 = $moc_loc_s2 = $moc_loc_s3a = $moc_loc_s3b = $moc_loc_s4 = $moc_loc_s5 = $moc_loc_s6 = "(Internet connection needed)";
     ?>
+    
     <body>        
         <div id="content" class="moc-sandbox">            
             <div class="moc-platform">
@@ -157,17 +60,17 @@
                             
                             <!--SLIDES-->
                             <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 1300px; height: 620px; overflow: hidden;">
+                                <div data-p="225.00" style="display: none;">                                    
+                                        <img data-u="image" src="img/UTBMOC_Banner_welcome.jpg" />
+                                </div>
                                 <div data-p="225.00" style="display: none;">
                                     <a href="http://pmutb.slack.com" target="_blank">
                                         <img data-u="image" src="img/UTBMOC_Banner_slack.jpg" />
                                     </a>
-                                </div>                                
+                                </div>
                                 <div data-p="225.00" style="display: none;">                                    
                                         <img data-u="image" src="img/UTBMOC_Banner_help.jpg" />
-                                </div>
-                                <div data-p="225.00" style="display: none;">                                    
-                                        <img data-u="image" src="img/UTBMOC_Banner_welcome.jpg" />
-                                </div>
+                                </div>    
                                 <div data-p="225.00" style="display: none;">
                                     <a href="img/UTBMOC_Banner_photocopy.jpg" data-lightbox="map">
                                         <img data-u="image" src="img/UTBMOC_Banner_photocopy.jpg" />
@@ -397,7 +300,7 @@
         <!-- Lightbox-->
         <script src="js/lightbox-plus-jquery.min.js"></script>
 
-        <!--Refresher-->
+        <!--AJAX Refresher-->
         <script>
             $(function() {
               $("#refresh").click(function(evt) {
@@ -406,7 +309,6 @@
               })
             })
         </script>
-
 
         <!--Cookie-->
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
@@ -438,8 +340,9 @@
           $.cookie.json = true;
           repopulateCheckboxes();
         </script>
-        <script>
-            
+
+        <!--Overscroll fix-->
+        <script>            
             $('body').on('touchmove', selScrollable, function(e) {
                 // Only block default if internal div contents are large enough to scroll
                 // Warning: scrollHeight support is not universal. (http://stackoverflow.com/a/15033226/40352)
@@ -448,8 +351,5 @@
                 }
             });
         </script>
-
-        
-        
     </body>
 </html>
