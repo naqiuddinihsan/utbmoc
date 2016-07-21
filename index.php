3f6054d69615ecb3401e30923957439270b987e7
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-07-21 16:07:02 +0800

    removed all php functions

--- index.php
+++ index.php
@@ -14,34 +14,6 @@
         <meta name="apple-mobile-web-app-status-bar-style" content="white">
     </head> 
     
-    <!--Location data-->
-    <?php
-//        if (include("res/moc_data.php"))
-//            include("res/moc_data.php");
-//        else
-//        $moc_loc_s1 = $moc_loc_s2 = $moc_loc_s3a = $moc_loc_s3b = $moc_loc_s4 = $moc_loc_s5 = $moc_loc_s6 = "(Internet connection needed)";
-        // Open Bank
-        $moc_loc_s1 = "Respective banks";
-
-        // Insurance
-        $moc_loc_s2 = "Concourse Hall";
-
-        //Student ID
-        $moc_loc_s3a = "???";
-
-        // Register course
-        $moc_loc_s3b = "Computer Labs(?)";
-
-        // Pay fees
-        $moc_loc_s4 = "GF.27/28";
-
-        // Collect orientation pack
-        $moc_loc_s5 = "Concourse Hall";
-
-        // CCA Booths
-        $moc_loc_s6 = "Teratak Putih";
-
-    ?>
     
     <body>        
        <p>Hellow world!</p>
