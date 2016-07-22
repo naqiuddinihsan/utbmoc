40d140ff35cebecb401b73ed4fe9c6c1624680d6
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-07-22 16:14:23 +0800

    manifest fix

--- res/moc_banner.php
+++ res/moc_banner.php
@@ -7,8 +7,8 @@
         moc_ldBanner("img/UTBMOC_Banner_welcome.jpg");
         moc_ldBanner("img/UTBMOC_Banner_slack.jpg");
         moc_ldBanner("img/UTBMOC_Banner_help.jpg");
-//        moc_ldBanner("img/UTBMOC_Banner_photocopy.jpg");
-//        moc_ldBanner("img/UTBMOC_Banner_passphot.jpg");
+        moc_ldBanner("img/UTBMOC_Banner_photocopy.jpg");
+        moc_ldBanner("img/UTBMOC_Banner_passphot.jpg");
 
     // Banner End, do not edit
     moc_prBannerFt();
