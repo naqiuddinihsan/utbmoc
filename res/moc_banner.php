60b7f158619d2845289827167cf678fe2b37149a
Ihsan <naqiuddin.ihsan@gmail.com>
2016-07-22 23:51:38 +0800

    Omiited photocopy & passphot banner from page

--- res/moc_banner.php
+++ res/moc_banner.php
@@ -17,8 +17,8 @@
         moc_ldBanner("img/UTBMOC_Banner_welcome.jpg");
         moc_ldBanner("img/UTBMOC_Banner_slack.jpg");
         moc_ldBanner("img/UTBMOC_Banner_help.jpg");
-        moc_ldBanner("img/UTBMOC_Banner_photocopy.jpg");
-        moc_ldBanner("img/UTBMOC_Banner_passphot.jpg");
+        // moc_ldBanner("img/UTBMOC_Banner_photocopy.jpg");
+        // moc_ldBanner("img/UTBMOC_Banner_passphot.jpg");
 ?>
                 
         </div>
