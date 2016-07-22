8a71c64f93f772d80d09d9450aa48723fc3355c5
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2016-07-22 15:55:15 +0800

    Added meaningful comments

--- res/moc_banner.php
+++ res/moc_banner.php
@@ -1,9 +1,15 @@
 <?php
+    // Banner Start, do not edit
     moc_prBannerHd();
+
+    // Banner Contents, they will appear sequentially
+
         moc_ldBanner("img/UTBMOC_Banner_welcome.jpg");
         moc_ldBanner("img/UTBMOC_Banner_slack.jpg");
         moc_ldBanner("img/UTBMOC_Banner_help.jpg");
 //        moc_ldBanner("img/UTBMOC_Banner_photocopy.jpg");
 //        moc_ldBanner("img/UTBMOC_Banner_passphot.jpg");
+
+    // Banner End, do not edit
     moc_prBannerFt();
 ?>
