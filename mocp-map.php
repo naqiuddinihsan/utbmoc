38c202e6e01aaf2c907ac717822b2e180bed8c07
naqiuddinihsan <naqiuddin.ihsan@gmail.com>
2017-07-22 12:58:32 +0800

    Compress photos & deleted unused jpegs

--- mocp-map.php
+++ mocp-map.php
@@ -3,13 +3,13 @@
         <li>
             <div class="moc-step">
                 <h4>Main Map</h4>
-                <img src="/img/utbmoc_mainmap.png" alt="Main Map" style="width:100%">
+                <img src="/img/utbmoc_mainmap.jpg" alt="Main Map" style="width:100%">
             </div>
         </li>
         <li>
             <div class="moc-step">
                 <h4>CCA Booths Map</h4>
-                <img src="/img/utbmoc_ccamap.png" alt="Main Map" style="width:100%">
+                <img src="/img/utbmoc_ccamap.jpg" alt="Main Map" style="width:100%">
             </div>
         </li>
     </ul>
